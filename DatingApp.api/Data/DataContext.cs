5f387fccc0153e0f9f7b93c4c5dc97e24b308730
vivek00012 <35500124+vivek00012@users.noreply.github.com>
2019-09-09 21:05:57 +0530

    FileCommit
    Commit #changes in configuration added initial migration

---
+++ DatingApp.api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.api.Data
+{
+    public class DataContext:DbContext
+    {
+  public DbSet<Value> Values{get;set;}
+  public DataContext(DbContextOptions<DataContext> options):base(options){}
+        
+        
+    }
+}
---
+++ DatingApp.api/Migrations/20190909145255_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.api.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+
+                    
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.Id);
+                });
+
+                migrationBuilder.Sql(@"INSERT into [Values] values ('value1'),('value2')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+    }
+}
